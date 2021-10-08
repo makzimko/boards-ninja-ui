e907a0033a5339189f484ee4f152f91bdc38a6d0
Maksym Tkachuk <uakorn@gmail.com>
2021-10-08 20:53:01 +0300

    Fix counter

--- src/ui/ListHeader/ListHeader.tsx
+++ src/ui/ListHeader/ListHeader.tsx
@@ -8,7 +8,7 @@ type ListHeaderProps = {
 const ListHeader = ({ title, counter }: ListHeaderProps) => {
     return <div className={styles.wrapper}>
         <div className={styles.title}>{title}</div>
-        {counter && <div className={styles.counter}>{counter} items</div>}
+        {!!counter && <div className={styles.counter}>{counter} items</div>}
     </div>
 };
 
