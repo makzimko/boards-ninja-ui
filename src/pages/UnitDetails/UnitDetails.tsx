dd4b1ea80a502f0a2bc2d7028e6a0b5d56bb4281
Maksym Tkachuk <uakorn@gmail.com>
2022-05-17 14:39:29 +0300

    Added units details without styles

--- src/pages/UnitDetails/UnitDetails.tsx
+++ src/pages/UnitDetails/UnitDetails.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useUnitDetailsActions from '../../atoms/unitDetails/actions';
+import { LOADING } from '../../atoms/loading';
+import { useRecoilValue } from 'recoil';
+import { unitDetailsState } from '../../atoms/unitDetails/atoms';
 
 const UnitDetails = () => {
   const { id } = useParams();
-  return <div>unit details {id} </div>;
+  const { fetch } = useUnitDetailsActions();
+  const [fetchStatus, setFetchStatus] = useState(LOADING.INITIAL);
+  const unitDetails = useRecoilValue(unitDetailsState);
+
+  useEffect(() => {
+    setFetchStatus(LOADING.PENDING);
+
+    if (id) {
+      fetch(id)
+        .then(() => setFetchStatus(LOADING.SUCCESS))
+        .catch(() => setFetchStatus(LOADING.ERROR));
+    }
+  }, [id, fetch]);
+
+  if (fetchStatus === LOADING.PENDING) {
+    return <div>loading</div>;
+  }
+
+  if (fetchStatus === LOADING.ERROR) {
+    return <div>error</div>;
+  }
+
+  return (
+    <div>
+      unit details {id} {JSON.stringify(unitDetails)}
+    </div>
+  );
 };
 
 export default UnitDetails;
